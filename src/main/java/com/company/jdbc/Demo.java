25ff56fab707f8bb83c63465852767e5eeba9584
rathodbalaji15 <113059676+rathodbalaji15@users.noreply.github.com>
2023-03-19 10:43:59 +0530

    Demo line 2

--- src/main/java/com/company/jdbc/Demo.java
+++ src/main/java/com/company/jdbc/Demo.java
@@ -3,5 +3,6 @@ package com.company.jdbc;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Demo code line 1");
+        System.out.println("Demo code line 2");
     }
 }
